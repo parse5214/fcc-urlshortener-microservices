ed8ef06ae6d11764a443d758eee70a573733e738
blueglass <cardanoviette123@gmail.com>
2022-08-18 18:10:29 -0500

    Update dependencies Update index Update README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# URL Shortener Microservice
+# URL Shortener Microservice from FCC
 
-This is the boilerplate code for the URL Shortener Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice.
+## Description
+
+This is the develop of the URL Shortener Microservice as part of the FreeCodeCamp curriculum.
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const url = require('url');
+const bodyParser = require('body-parser');
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true,
+useUnifiedTopology: true});
+
+const schema = new mongoose.Schema({url: 'string'});
+const Url = mongoose.model('Url', schema);
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(cors());
 
@@ -15,6 +26,29 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
+app.post('/api/shorturl', function(req, res) {
+  const bodyUrl = req.body.url;
+  dns.lookup(url.parse(bodyUrl).hostname, function(err, address) {
+    if(!address) res.json({error: 'invalid url'});
+    else{
+      const originalUrl = new Url({url: bodyUrl});
+      originalUrl.save(function(err, data) {
+        res.json({
+          original_url: data.url,
+          short_url: data.id
+        });
+      });
+    }
+  });
+});
+
+app.get('/api/shorturl/:short_url', function(req, res) {
+  Url.findById(req.params.short_url, function(err, data) {
+    if(!data) res.json({error: 'invalid_url'});
+    else res.redirect(data.url);
+  });
+});
+
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
